471a08b3443f6a594d5f0eb0b0af25b33da88fb9
IvanPaskalev <93085542+IvanPaskalev@users.noreply.github.com>
2022-07-09 18:37:30 +0300

    tggt
    tgtggg

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -15,7 +15,7 @@ namespace projectApp.Controllers
         [HttpGet]
         public String Get()
         {
-          return "Hello Bitches";
+          return "Hello Bitches 2";
 
         }
     }
